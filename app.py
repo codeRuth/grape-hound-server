49e7c17763a18a3fdb781368457f114d20a10380
Ruthvik Vijayakumar <coderuth@outlook.com>
2020-07-11 05:11:27 +0530

    added file save support

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify, request
+import json
+import base64, os
+import uuid
+
 app = Flask(__name__)
 
+FILE_UPLOAD_DIR = '/uploads/'
 
 @app.route('/')
 def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
+    json_data = {'message': 'Hello World!'}
     return jsonify(json_data)
 
 @app.route('/test')
@@ -14,9 +18,15 @@ def test():
 
 @app.route('/process', methods=["POST"])
 def process():
-    print(dict(request.data))
-    return jsonify({"message": "Successful"})
+    data = json.loads(request.data)
+    imageData = data['img']
+    type = data['type']
+    image = base64.b64decode(str(imageData))
+    file_name = str(uuid.uuid4().hex)
+    with open(os.path.join(os.path.dirname(__file__) + FILE_UPLOAD_DIR, file_name + ".png"), 'wb+') as f:
+        f.write(image)
+    return jsonify({"message": "Image saved Successful"})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", debug = True)
